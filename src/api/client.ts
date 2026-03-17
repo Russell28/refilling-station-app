75c7e54b5d2733344221365ad782a37ed2217647
Russell28 <russellsalioan@gmail.com>
2026-03-17 15:35:53 +0800

    WR-11 chore: setup api base url and client

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5173/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:7034/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+console.log("API Base URL:", import.meta.env.VITE_API_BASE_URL);
