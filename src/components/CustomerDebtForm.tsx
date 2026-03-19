2475a2d28849098785d6b549c69d687498481e00
Russell28 <russellsalioan@gmail.com>
2026-03-19 17:08:19 +0800

    WR-14 feat(app): add CustomerDebtForm layout

--- src/components/CustomerDebtForm.tsx
+++ src/components/CustomerDebtForm.tsx
@@ -1,8 +1,39 @@
 
 export default function CustomerDebtForm() {
+    function handleSubmit(event: React.SubmitEvent<HTMLFormElement>) {
+        event.preventDefault();
+    }
+
     return (
-        <div>
-            <h2>New Customer Debt Entry</h2>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Date</label>
+                <input type="date" />
+            </div>
+
+            <div>
+                <label>Customer Name</label>
+                <input type="text" />
+            </div>
+
+            <div>
+                <label>Qty</label>
+                <input type="number" />
+            </div>
+
+            <div>
+                <label>Amount</label>
+                <input type="number" />
+            </div>
+
+            <div>
+                <label>Notes</label>
+                <input type="text" />
+            </div>
+
+            <div style={{ marginTop: 12 }}>
+                <button type="submit">Save</button>
+            </div>
+        </form>
     );
 }
